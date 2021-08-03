b7e232ba10a0f44a07acc67e99839747a751f6ef
RADAJO2 <radajo2@oocl.com>
2021-08-04 00:05:34 +0800

    [POSV2] [JOANNA] feat: add convertToItems

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -17,6 +17,23 @@ public class PosMachine {
         return Collections.frequency(ItemDataLoader.loadBarcodes(), currentItemBarcode);
     }
 
- 
+    private List<Item> convertToItems(List<String> barcodes) {
+        LinkedList<Item> items = new LinkedList<Item>();
+        List<ItemInfo> itemInfo = loadAllItemsInfo();
+        barcodes = new ArrayList<>(new LinkedHashSet<>(barcodes));
+
+        for (String barcode : barcodes) {
+            Item itemValue = new Item();
+            for (ItemInfo itemInfoVal : itemInfo) {
+                if (barcode.equals(itemInfoVal.getBarcode())) {
+                    itemValue.setName(itemInfoVal.getName());
+                    itemValue.setPriceUnit(itemInfoVal.getPrice());
+                    itemValue.setQuantity(retrieveItemCount(barcode));
+                }
+            }
+            items.add(itemValue);
+        }
+        return items;
+    }
 
 }
